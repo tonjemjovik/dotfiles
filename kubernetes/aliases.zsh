073b7c1d4a41851ecd8a7925defea270797c9fc4
Geir Andre Lund <geir.andre.lund@nav.no>
2019-05-31 10:01:35 +0200

    update - alias k is kubectl kdev

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -1,2 +1,3 @@
-alias k='HTTPS_PROXY="socks5://localhost:14122" NO_PROXY=".microsoftonline.com" kubectl --context=dev-fss'
+alias kdev='HTTPS_PROXY="socks5://localhost:14122" NO_PROXY=".microsoftonline.com" kubectl --context=dev-fss'
 alias kprod='HTTPS_PROXY="socks5://localhost:14122" NO_PROXY=".microsoftonline.com" kubectl --context=prod-fss'
+alias k='HTTPS_PROXY="socks5://localhost:14122" NO_PROXY=".microsoftonline.com" kubectl'
