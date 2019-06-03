1f14be1ea8a7518dcb4d40b2a0858ea4b441b22f
Geir Andre Lund <geir.andre.lund@nav.no>
2019-06-03 10:57:37 +0200

    Specify https_proxy when talking to kubectl api

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -1,3 +1,6 @@
-alias kdev='HTTPS_PROXY="socks5://localhost:14122" NO_PROXY=".microsoftonline.com" kubectl --context=dev-fss'
-alias kprod='HTTPS_PROXY="socks5://localhost:14122" NO_PROXY=".microsoftonline.com" kubectl --context=prod-fss'
-alias k='HTTPS_PROXY="socks5://localhost:14122" NO_PROXY=".microsoftonline.com" kubectl'
+alias with_proxy='https_proxy=socks5://localhost:14122 http_proxy=socks5://localhost:14122 no_proxy=".microsoftonline.com"'
+alias wp='with_proxy'
+
+alias kdev='wp kubectl --context=dev-fss'
+alias kprod='wp kubectl --context=prod-fss'
+alias k='wp kubectl'
