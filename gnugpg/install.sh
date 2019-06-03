b22d7a47444c6fe8d5d958ac94acc8272ec35da7
Geir Andre Lund <geir.andre.lund@nav.no>
2019-06-03 13:08:49 +0200

    add gpg conf

--- gnugpg/install.sh
+++ gnugpg/install.sh
@@ -3,20 +3,20 @@
 
 if [[ -d ~/.gnupg/gpg.conf ]]; then
     printf "Updating gpg.conf \n"
-   cat gpg.conf > ~/.gnupg/gpg.conf
+   cat gnugpg/gpg.conf > ~/.gnupg/gpg.conf
 
 else
      printf "Installing gpg.conf \n"
     touch ~/.gnupg/gpg.conf
-    cat gpg.conf > ~/.gnupg/gpg.conf
+    cat  gnugpg/gpg.conf > ~/.gnupg/gpg.conf
 fi
 
 if [[ -d ~/.gnupg/gpg-agent.conf ]]; then
     printf "Updating gpg-agent.conf \n"
-   cat gpg-agent.conf > ~/.gnupg/gpg-agent.conf
+   cat  gnugpg/gpg-agent.conf > ~/.gnupg/gpg-agent.conf
 
 else
      printf "Installing gpg-agent.conf\n"
     touch ~/.gnupg/gpg-agent.conf
-    cat gpg-agent.conf > ~/.gnupg/gpg-agent.conf
+    cat gnugpg/gpg-agent.conf > ~/.gnupg/gpg-agent.conf
 fi
