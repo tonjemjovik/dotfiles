e94e88685d050cb59e863139cc12893c2fd0d5be
Geir André Lund <geir.andre.lund@nav.no>
2019-11-04 10:50:20 +0100

    More stuff

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -2,3 +2,8 @@
 alias kdev='kubectl --context=dev-fss'
 alias kprod='kubectl --context=prod-fss'
 alias k='kubectl --context=preprod-fss'
+
+
+alias pods='{ name=$(cat -); kubectl get pods --all-namespaces -lapp=$name; }<<<'
+alias dpods='{ name=$(cat -); k get pods  --all-namespaces -lapp=$name; }<<<'
+alias prods='{ name=$(cat -); kprod get pods  --all-namespaces -lapp=$name; }<<<'
