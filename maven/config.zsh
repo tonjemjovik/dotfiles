3f678046572adb390b2b1a0fdfc23b054b53d804
Geir André Lund <geir.andre.lund@nav.no>
2019-11-20 07:52:24 +0100

    locale settings + misc

--- maven/config.zsh
+++ maven/config.zsh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-export MAVEN_OPTS="-DsocksProxyHost=localhost -DsocksProxyPort=14122"
+export MAVEN_OPTS="-DsocksProxyHost=127.0.0.1 -DsocksProxyPort=14122 -DsocksNonProxyHosts=127.0.0.1|dl.bintray.com|repo.maven.apache.org"
