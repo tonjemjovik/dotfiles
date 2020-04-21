7aa093d49ef4fb0af1f0ece7dd07914e232fe273
Geir André Lund <geir.andre.lund@nav.no>
2020-04-21 07:48:06 +0200

    Update nvm

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,8 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+
+# partial completion suggestions
+zstyle ':completion:*' list-suffixes
+zstyle ':completion:*' expand prefix suffix
