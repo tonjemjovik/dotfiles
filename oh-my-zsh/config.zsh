bbc505bb08ab566a35cd67f93161add9bec3da97
Geir André Lund <geir.andre.lund@nav.no>
2021-01-13 19:57:25 +0100

    Bytta oh-my-zsh theme 🤓

--- oh-my-zsh/config.zsh
+++ oh-my-zsh/config.zsh
@@ -1,18 +1,15 @@
 export ZSH=~/.oh-my-zsh
-ZSH_THEME="af-magic"
+#ZSH_THEME="af-magic"
+ZSH_THEME="agnoster"
 
 plugins=(
   git
 )
-
 source ${ZSH}/oh-my-zsh.sh
-
 # Syntaks
 source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 # Autocomplete
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-
 # kubectx on prompt
 source "/usr/local/opt/kube-ps1/share/kube-ps1.sh"
 PROMPT='$(kube_ps1)'${PROMPT}
