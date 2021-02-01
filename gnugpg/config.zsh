19cc72e9823859c1cbb8fc88a637625c9e968fb8
Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
2021-02-01 14:11:39 +0100

    tilpassninger

--- gnugpg/config.zsh
+++
@@ -1,4 +0,0 @@
-GPG_TTY=$(tty)
-export GPG_TTY
-echo "Starting gpg-agent daemon"
-eval $(gpg-agent --daemon)
--- vault/config.zsh
+++ vault/config.zsh
@@ -2,7 +2,7 @@ autoload -U +X bashcompinit && bashcompinit
 complete -o nospace -C /usr/local/bin/vault vault
 
 # Vault
-export VAULT_ADDR=https://vault.adeo.no  USER=L153963
+export VAULT_ADDR=https://vault.adeo.no  USER=M152447
 
 
 alias v='vault'
